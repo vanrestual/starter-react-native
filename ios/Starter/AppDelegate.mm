66a96f668fcceb712542d007fe16e7e25d4bfff3
Ivan Restu Alfiansyah <vanrestual@gmail.com>
2023-06-07 05:11:06 +0700

    sign in with google

--- ios/Starter/AppDelegate.mm
+++ ios/Starter/AppDelegate.mm
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <RNGoogleSignin/RNGoogleSignin.h>
 
 @implementation AppDelegate
 
@@ -31,6 +32,10 @@
 - (BOOL)concurrentRootEnabled
 {
   return true;
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(nonnull NSURL *)url options:(nonnull NSDictionary<NSString *,id> *)options {
+  return [RNGoogleSignin application:application openURL:url options:options];
 }
 
 @end
